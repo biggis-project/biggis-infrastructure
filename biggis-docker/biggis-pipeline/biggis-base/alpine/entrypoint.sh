781c4dd4add5a75b33fbc90ab894b9a10ebb29e0
wipatrick <patrick.wiener@hs-karlsruhe.de>
2016-07-20 16:58:16 +0200

    Added minimal biggis/base:alpine-3.4 image with UID handling

---
+++ biggis-docker/biggis-pipeline/biggis-base/alpine/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Add local user
+# Either use the USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${USER_ID:-9001}
+USER=${USER_NAME:-biggis}
+
+echo "Starting with UID : $USER_ID"
+addgroup -S -g $USER_ID $USER
+adduser -h /home/$USER -u $USER_ID -s /bin/sh -G $USER -S $USER
+chown -R $USER:$USER /home/$USER /opt /tmp /storage
+export HOME=/home/$USER
+
+exec gosu $USER "$@"
